00b978b7cd93725385890b5112206efda79a7f72
8660306843 <80092002+8660306843@users.noreply.github.com>
2021-03-10 17:52:04 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+function validate()
+{
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+
+    if(username=="admin" && password=="user")
+    {
+        alert("login page");
+        return false;
+    }
+    else
+    {
+        alert("login failed");
+    }
+}
